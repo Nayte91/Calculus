4af52d64042bc2d2324ee3fcefe3a8dae1f7e4d9
Nayte <nayte91@gmail.com>
2021-08-10 23:41:13 +0200

    Calculator and tests OK

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -2,18 +2,24 @@
 
 namespace App\Controller;
 
-use App\Service\Calculator;
+use App\Service\CalculatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class APIController extends AbstractController
 {
     #[Route(path:'/computation', name: 'computation', methods: ['POST'])]
-    public function compute(Calculator $calculator): Response
+    public function compute(CalculatorInterface $calculator, Request $request): Response
     {
+        $decoded = '';
+        if ($request->getContent()) {
+            $decoded = json_decode($request->getContent(), true)['entry'];
+        }
+
         return $this->json([
-            'result' => $calculator->getResult()
+            'result' => (string) $calculator->compute($decoded)
         ]);
     }
 }
